9bb6638913a849e0af2f4dc0824a5bf0a58cd6b1
Tore Brandtzæg <toreb27@gmail.com>
2021-06-10 11:29:33 +0200

    Changed the implementation of the articles pagination support.

--- src/main/java/no/toreb/nrknewsconsumer/controller/ArticleController.java
+++ src/main/java/no/toreb/nrknewsconsumer/controller/ArticleController.java
@@ -3,8 +3,6 @@ package no.toreb.nrknewsconsumer.controller;
 import lombok.RequiredArgsConstructor;
 import no.toreb.nrknewsconsumer.model.Article;
 import no.toreb.nrknewsconsumer.repository.ArticleRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +18,8 @@ class ArticleController {
     private final ArticleRepository articleRepository;
 
     @GetMapping
-    public List<Article> getAll(@RequestParam(value = "page", defaultValue = "0") final int page,
-                                @RequestParam(value = "size", defaultValue = "10") final int size) {
-        return articleRepository.findAll(PageRequest.of(page, size, Direction.ASC, "publishedAt"))
-                                .getContent();
+    public List<Article> getAll(@RequestParam(value = "page", defaultValue = "0") final long page,
+                                @RequestParam(value = "size", defaultValue = "10") final long size) {
+        return articleRepository.findAll(size, page * size);
     }
 }
--- src/main/java/no/toreb/nrknewsconsumer/repository/ArticleRepository.java
+++ src/main/java/no/toreb/nrknewsconsumer/repository/ArticleRepository.java
@@ -1,9 +1,9 @@
 package no.toreb.nrknewsconsumer.repository;
 
 import no.toreb.nrknewsconsumer.model.Article;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +11,11 @@ import java.util.List;
 
 @Repository
 @Transactional
-public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
     @Query("select distinct article_id from article")
     List<String> distinctArticleIds();
 
-    @Override
-    List<Article> findAll(Sort sort);
+    @Query("select * from article order by PUBLISHED_AT limit :limit offset :offset")
+    List<Article> findAll(@Param("limit") long limit, @Param("offset") long offset);
 }
