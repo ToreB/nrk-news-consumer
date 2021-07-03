1cc55e11b824bc551970a71be163c28968ff5e49
Tore Brandtzæg <toreb27@gmail.com>
2021-07-04 00:15:50 +0200

    Fixed update in migration script.

--- src/main/resources/db/migration/V001.003__ALTER_HIDDEN_ARTICLES.sql
+++ src/main/resources/db/migration/V001.003__ALTER_HIDDEN_ARTICLES.sql
@@ -1,5 +1,5 @@
 alter table hidden_articles add column hidden_at text;
 
 update hidden_articles
-set hidden_at = (select published_at from article a where a.article_id = article)
+set hidden_at = (select published_at from article a where a.gen_id = article)
 where hidden_at is null;
