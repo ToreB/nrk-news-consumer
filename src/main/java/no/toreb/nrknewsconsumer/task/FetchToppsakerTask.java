6e9f7d39d9fc8e704a9e3d459fbd36d1c54858dd
Tore Brandtzæg <toreb27@gmail.com>
2021-07-11 22:09:41 +0200

    The FetchToppsakerTask now takes into consideration OS sleep / hibernation.
    Separated the task scheduling and should fetch calculation, to make it
    fetch shortly after the OS wakes up from sleep or hibernation.
    The frequency of the scheduling of the task has been increased to make
    this possible, but the rate of fetching is the same as before.

--- src/main/java/no/toreb/nrknewsconsumer/task/FetchToppsakerTask.java
+++ src/main/java/no/toreb/nrknewsconsumer/task/FetchToppsakerTask.java
@@ -9,6 +9,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,19 +24,29 @@ class FetchToppsakerTask {
     private final ArticleFetcher articleFetcher;
 
     private final String articlesFeedUrl;
+    
+    private final Duration syncRateDuration;
+
+    private LocalDateTime lastFetchTime;
 
     public FetchToppsakerTask(final ArticleRepository articleRepository,
                               final ArticleFetcher articleFetcher,
-                              @Value("${task.fetch-toppsaker.articles-feed-url}") final String articlesFeedUrl) {
+                              @Value("${task.fetch-toppsaker.articles-feed-url}") final String articlesFeedUrl,
+                              @Value("${task.fetch-toppsaker.fetch-rate}") final Duration syncRateDuration) {
         this.articleRepository = articleRepository;
         this.articleFetcher = articleFetcher;
         this.articlesFeedUrl = articlesFeedUrl;
+        this.syncRateDuration = syncRateDuration;
     }
 
     @Transactional
     @Scheduled(fixedDelayString = "${task.fetch-toppsaker.fixed-delay}",
                initialDelayString = "${task.fetch-toppsaker.initial-delay}")
     public void run() {
+        if (!shouldFetch()) {
+            return;
+        }
+        
         final List<Article> articles = articleFetcher.fetch(articlesFeedUrl);
 
         final List<String> existingArticleIds = articleRepository.distinctArticleIds();
@@ -46,5 +58,23 @@ class FetchToppsakerTask {
 
         log.info("Found {} new news articles", newArticles.size());
         newArticles.forEach(articleRepository::save);
+        lastFetchTime = LocalDateTime.now();
+    }
+
+    /**
+     * By checking whether to fetch as a separate calculation from the task scheduling, combined with a rather frequent
+     * scheduling of the task, the task should be able to better handling if the operating system frequently goes into
+     * sleep or hibernation.
+     * This should make the task fetch shortly after the OS wakes up.
+     * 
+     * @return true if should sync, false otherwise.
+     */
+    private boolean shouldFetch() {
+        if (lastFetchTime == null) {
+            return true;
+        }
+
+        final LocalDateTime nextSyncTime = lastFetchTime.plusSeconds(syncRateDuration.toSeconds());
+        return LocalDateTime.now().compareTo(nextSyncTime) >= 0;
     }
 }
